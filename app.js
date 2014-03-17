f356209aab461719b5d3916f2e692e21ec3dd2ab
sdrothco <sdrothco@gmail.com>
2014-03-17 15:19:54 -0600

    Refactored to add function call, prompt for number, and error checking.

--- app.js
+++ app.js
@@ -1,28 +1,75 @@
 $(document).ready(function() {
 
-	for( var i=1; i<=100; i++) {
+	var doFizzBuzzCounting = function (maxNumber) {
+		for( var i=1; i<=maxNumber; i++) {
 
-		var stringToAppend = i;
-		
-		if( i % 3 === 0 ) {
-			stringToAppend = "fizz";
+			var stringToAppend = i;
+			if( i % 3 === 0 ) {
+				stringToAppend = "fizz";
+			}
+			if( i % 5 === 0 ) {
+				stringToAppend = "buzz";
+			}
+			if( i % 3 === 0 && i % 5 === 0 ) {
+				stringToAppend = "fizz buzz";
+			}
+			
+			if (i !== maxNumber) {
+				stringToAppend += ", ";
+			}
+			if( i % 10 === 0 ) {
+				stringToAppend += "<br>";
+			}
+
+			$('body').append(stringToAppend);
 		}
-		if( i % 5 === 0 ) {
-			stringToAppend = "buzz";
+	};
+
+
+	var maxNum;
+	var isValidNum = false;
+	var errorStr = "";
+	do {
+
+		maxNum = prompt ("Enter a whole number (1-300) to count up to:");
+		console.log("Entered number was: " + maxNum);
+
+		if (maxNum === null ) {
+			break;
 		}
-		if( i % 3 === 0 && i % 5 === 0 ) {
-			stringToAppend = "fizz buzz";
+		if (typeof maxNum === "undefined" || maxNum.trim() === "") {
+			errorStr = "You entered an empty or undefined string.\n";
+			console.log(errorStr);
 		}
-		
-		if (i !== 100) {
-			stringToAppend += ", ";
+		else {
+			maxNum = +maxNum;
+			if (isNaN(maxNum) || maxNum % 1 !== 0) {
+				errorStr = "You entered an invalid number (NaN) or decimal value.\n";
+				console.log(errorStr);
+			}
+			else if (maxNum < 1 || maxNum > 300) {
+				errorStr = "You entered a number that was not within range of 1 -300.\n";
+				console.log(errorStr);
+			}
+			else {
+				// It was a good number!
+				isValidNum = true;
+			}
 		}
 
-		if( i % 10 === 0 ) {
-			stringToAppend += "<br>";
+		if (!isValidNum) {
+			alert (errorStr + "Please enter a valid integer number between 1 and 300.");
 		}
 
-		$('body').append(stringToAppend);
+	} while ( !isValidNum );
+
+	if (isValidNum) {
+		doFizzBuzzCounting(maxNum);
 
 	}
+	else {
+			$('body').append("Stopping FizzBuzz.");
+	}
+
 });
+
